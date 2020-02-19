f0d8c0ebdda120c328f2a2ecc7336222b542e230
ambroslins <ambroslins@users.noreply.github.com>
2020-02-19 18:53:13 +0100

    add nextVariant function

--- src/Jasskell/Variant.hs
+++ src/Jasskell/Variant.hs
@@ -1,8 +1,13 @@
 module Jasskell.Variant where
 
-import Jasskell.Card.Suit
+import           Jasskell.Card.Suit
 
 data Variant = Trump Suit | Direction Direction | Slalom Direction
     deriving (Eq, Show)
 
 data Direction = BottomUp | TopDown deriving (Eq, Show)
+
+nextVariant :: Variant -> Variant
+nextVariant (Slalom TopDown ) = Slalom BottomUp
+nextVariant (Slalom BottomUp) = Slalom TopDown
+nextVariant v                 = v
