618ffd7bc700ee30c33063a8484dc27e0e57bbd9
ambroslins <ambroslins@users.noreply.github.com>
2020-03-27 23:52:22 +0100

    add Round to Game data

--- src/Jasskell/Game.hs
+++ src/Jasskell/Game.hs
@@ -3,6 +3,10 @@ module Jasskell.Game where
 import           Data.Finite
 import           Data.Vector.Sized
 import           Jasskell.Player
+import           Jasskell.Round
 
 data Game n = Game { players :: Vector n Player
-                   , currentIndex :: Finite n }
+                   , currentIndex :: Finite n
+                   , currentRound :: Round n
+                   , rounds :: [RoundFinished n]
+                   }
