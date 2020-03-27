0a7a10dbcbd6bfe4b284589a0885f5335b03962a
ambroslins <ambroslins@users.noreply.github.com>
2020-03-27 23:42:07 +0100

    add Round data

---
+++ src/Jasskell/Round.hs
@@ -0,0 +1,24 @@
+module Jasskell.Round where
+
+import           Data.Vector.Sized
+import           Jasskell.Card
+import           Jasskell.Trick
+import           Jasskell.Variant
+import           GHC.TypeLits
+
+data RoundPlaying n = RoundPlaying { variant :: Variant
+                     , trick :: Trick n
+                     , tricks :: [TrickResolved n]
+                     }
+
+data RoundFinished n = RoundFinished (Vector n Int)
+
+data Round n = Starting
+           | Playing (RoundPlaying n)
+           | Finished (RoundFinished n)
+
+playCard :: KnownNat n => Card -> RoundPlaying n -> RoundPlaying n
+playCard c r = case trick r of
+    Unresolved t -> r { trick = addCard c t }
+    Resolved   _ -> error "start next trick"
+
