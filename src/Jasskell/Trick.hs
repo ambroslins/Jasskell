e5bfdc0577cfcb6615c5d248e034cdc58090aa69
ambroslins <ambroslins@users.noreply.github.com>
2020-03-29 18:59:52 +0200

    refactor Trick
    also fix rotation direction on resolving

--- src/Jasskell/Trick.hs
+++ src/Jasskell/Trick.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Jasskell.Trick where
 
 import           Data.Finite
 import qualified Data.Vector.Sized             as Vector
 import           Data.Vector.Sized              ( Vector )
 import           Jasskell.Card
-import           Jasskell.Card.Suit
 import           GHC.TypeLits
 
 data Resolved
@@ -12,19 +13,20 @@ data Unresolved
 
 data TrickUnresolved n = TrickUnresolved (Finite n) [Card] deriving Show
 
-data TrickResolved n = TrickResolved Suit (Vector n Card) deriving Show
+data TrickResolved n = TrickResolved (Finite n) (Vector n Card) deriving Show
 
 data Trick n = Unresolved (TrickUnresolved n)
                    | Resolved (TrickResolved n)
                    deriving Show
 
 addCard :: KnownNat n => Card -> TrickUnresolved n -> Trick n
-addCard c (TrickUnresolved f cs) = case Vector.fromListN t of
-  Just v  -> Resolved $ TrickResolved (suit $ head cs') v
+addCard c (TrickUnresolved f cs) = case Vector.fromListN cs' of
+  Just v  -> Resolved $ TrickResolved f $ rotateN (-toInteger f) v
   Nothing -> Unresolved $ TrickUnresolved f cs'
- where
-  cs' = cs ++ [c]
-  t   = (drop <> take) (fromInteger $ getFinite f) cs'
+  where cs' = cs ++ [c]
 
 newTrick :: Finite n -> TrickUnresolved n
 newTrick f = TrickUnresolved f []
+
+rotateN :: (KnownNat n) => Integer -> Vector n a -> Vector n a
+rotateN n vec = Vector.generate (Vector.index vec . (+ modulo n))
