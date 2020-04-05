2e684bc4b12f0243b053aa0399a5cad88a09fdf4
ambroslins <ambroslins@users.noreply.github.com>
2020-04-05 15:33:49 +0200

    add basic websocket user

--- server/src/Jasskell/User.hs
+++ server/src/Jasskell/User.hs
@@ -4,9 +4,15 @@ module Jasskell.User
     , getAction
     , putMessage
     , cliUser
+    , websocketUser
     )
 where
 
+import           Data.Aeson
+import           Network.WebSockets             ( Connection
+                                                , receiveData
+                                                , sendTextData
+                                                )
 import           Jasskell.Action
 import           Jasskell.Message
 
@@ -20,3 +26,11 @@ cliUser n = User { name       = n
                  , getAction  = PlayCard . read <$> getLine
                  , putMessage = \(UpdateGameView gv) -> print gv
                  }
+
+websocketUser :: Connection -> String -> User
+websocketUser c n = User
+    { name       = n
+    , getAction  = get
+    , putMessage = \(UpdateGameView g) -> sendTextData c $ encode g
+    }
+    where get = decode <$> receiveData c >>= \a -> maybe get return a
