6a052f0f62884a590da282bd96315208a9e16d65
ambroslins <ambroslins@users.noreply.github.com>
2020-04-13 20:44:01 +0200

    add stricter Message type

--- client/src/typescript/index.ts
+++ client/src/typescript/index.ts
@@ -1,23 +1,34 @@
+type Message =
+    {
+        type: 'connect',
+        address: string
+    } | {
+        type: 'send',
+        data: any
+    } | {
+        type: 'close'
+    }
+
 function handleSocketPort(port: any) {
     let socket: WebSocket | null = null;
-    return (event: { type: string, address?: string, data?: string }) => {
-        switch (event.type) {
-            case "connect":
+    return (message: Message) => {
+        switch (message.type) {
+            case 'connect':
                 if (socket === null || socket.readyState === WebSocket.CLOSED) {
-                    socket = new WebSocket(event.address);
-                    socket.onopen = (_) => port.send({ type: "open" });
-                    socket.onmessage = (ev) => port.send({ type: "message", data: ev.data });
-                    socket.onclose = (_) => port.send({ type: "close" });
-                    socket.onerror = (_) => port.send({ type: "error" });
+                    socket = new WebSocket(message.address);
+                    socket.onopen = (_) => port.send({ type: 'open' });
+                    socket.onmessage = (ev) => port.send({ type: 'message', data: ev.data });
+                    socket.onclose = (_) => port.send({ type: 'close' });
+                    socket.onerror = (_) => port.send({ type: 'error' });
                 }
 
                 break;
-            case "send":
+            case 'send':
                 if (socket && socket.readyState === WebSocket.OPEN) {
-                    socket.send(JSON.stringify(event.data));
+                    socket.send(JSON.stringify(message.data));
                 }
                 break;
-            case "close":
+            case 'close':
                 if (socket && socket.readyState === WebSocket.OPEN) {
                     socket.close();
                 }
