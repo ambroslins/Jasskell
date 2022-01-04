e091f233a19c6fac5a17a7d920e498d53f27e14c
ambroslins <ambros.lins@gmail.com>
2022-01-04 22:01:24 +0100

    add basic Variant module

---
+++ src/Variant.hs
@@ -0,0 +1,18 @@
+module Variant where
+
+import Suit
+
+data Variant
+  = Trump Suit
+  | Direction Direction
+  | Slalom Direction
+  deriving (Eq, Show)
+
+data Direction = TopDown | BottomUp
+  deriving (Eq, Show)
+
+next :: Variant -> Variant
+next = \case
+  Slalom TopDown -> Slalom BottomUp
+  Slalom BottomUp -> Slalom TopDown
+  variant -> variant
