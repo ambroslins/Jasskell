f72892b47ca9169171bd772d1e0a5e417dcba874
ambroslins <ambros.lins@gmail.com>
2022-01-17 23:37:20 +0100

    add basic Tick type

---
+++ src/Trick.hs
@@ -0,0 +1,39 @@
+module Trick
+  ( Trick,
+    variant,
+    leader,
+    cards,
+    make,
+    winner,
+    value,
+  )
+where
+
+import Card (Card)
+import Card qualified
+import Data.Finite
+import Data.Vector.Sized (Vector)
+import Data.Vector.Sized qualified as Vector
+import GHC.TypeNats (type (+))
+import Variant (Variant)
+
+data Trick n = Trick
+  { variant :: Variant,
+    leader :: Finite n,
+    cards :: Vector n Card
+  }
+  deriving (Show)
+
+make :: KnownNat n => Variant -> Finite n -> [Card] -> Maybe (Trick n)
+make var l cs = Trick var l . rotate (negate l) <$> Vector.fromList cs
+
+winner :: (KnownNat n, n ~ (m + 1)) => Trick n -> Finite n
+winner Trick {leader, cards, variant} = Vector.maxIndexBy (Card.compare variant lead) cards
+  where
+    lead = Card.suit $ Vector.index cards leader
+
+value :: Trick n -> Int
+value Trick {cards, variant} = sum $ Vector.map (Card.value variant) cards
+
+rotate :: KnownNat n => Finite n -> Vector n a -> Vector n a
+rotate n v = Vector.generate (\i -> Vector.index v $ i + n)
