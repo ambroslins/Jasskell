965aa3a9d6e8e08b60826c640abd427dcdb203b8
ambroslins <ambros.lins@gmail.com>
2022-02-17 22:04:25 +0100

    refactor Round

--- src/Round.hs
+++ src/Round.hs
@@ -1,12 +1,10 @@
-{-# LANGUAGE FlexibleContexts #-}
-
 module Round
-  ( Round (hands, leader, cards, tricks, variant),
-    Record,
+  ( Round,
     Error (..),
-    Result (..),
+    Record,
     playCard,
     current,
+    chooseVariant,
   )
 where
 
@@ -19,7 +17,6 @@ import Data.Vector.Sized qualified as Vector
 import JassNat (JassNat)
 import Relude.Extra.Lens (over)
 import Round.Record (Record)
-import Round.Record qualified as Record
 import Trick (Trick)
 import Trick qualified
 import Variant (Variant)
@@ -29,58 +26,72 @@ import Prelude hiding (round)
 data Round n = Round
   { hands :: Vector n Cards,
     leader :: Finite n,
+    phase :: Phase n
+  }
+  deriving (Show)
+
+data Phase n
+  = Choosing
+  | Playing (RoundPlaying n)
+  deriving (Show)
+
+data RoundPlaying n = RoundPlaying
+  { variant :: Variant,
     cards :: [Card],
-    tricks :: [Trick n],
-    variant :: Variant
+    tricks :: [Trick n]
   }
   deriving (Show)
 
 data Error
-  = NotYourTurn
+  = ChooseVariantFirst
+  | VariantAlreadyDefined
   | CardUnplayable Card.Reason
   deriving (Show)
 
-data Result n
-  = Ok (Round n)
-  | Closed (Record n)
-  | Error Error
-  deriving (Show)
-
-playCard :: JassNat n => Finite n -> Card -> Round n -> Result n
-playCard player card round
-  | player /= current round = Error NotYourTurn
-  | otherwise = case Card.status (variant round) (cards round) (Vector.index (hands round) player) card of
-    Card.Unplayable reason -> Error (CardUnplayable reason)
-    Card.Playable ->
-      case Trick.make (variant round) (leader round) cards' of
-        Nothing ->
-          Ok $
-            Round
-              { hands = hands',
-                leader = leader round + 1,
-                cards = cards',
-                tricks = tricks round,
-                variant = variant round
-              }
-        Just trick ->
-          maybe
-            (Ok round')
-            (Closed . Record.make)
-            (Vector.fromList $ reverse tricks')
-          where
-            winner = Trick.winner trick
-            tricks' = trick : tricks round
-            round' =
-              Round
-                { hands = hands',
-                  leader = winner,
-                  cards = [],
-                  tricks = tricks',
-                  variant = Variant.next $ variant round
-                }
-      where
-        cards' = cards round ++ [card]
-        hands' = over (Vector.ix player) (Set.delete card) (hands round)
-
 current :: KnownNat n => Round n -> Finite n
-current r = leader r + modulo (toInteger $ length $ cards r)
+current Round {leader, phase} = case phase of
+  Choosing -> leader
+  Playing RoundPlaying {cards} -> leader + modulo (toInteger $ length cards)
+
+playCard :: JassNat n => Card -> Round n -> Either Error (Round n)
+playCard card round@Round {hands, leader, phase} = case phase of
+  Choosing -> Left ChooseVariantFirst
+  Playing playing@RoundPlaying {variant, cards, tricks} ->
+    case Card.status variant cards (Vector.index hands player) card of
+      Card.Unplayable reason -> Left (CardUnplayable reason)
+      Card.Playable ->
+        pure $ case Trick.make variant leader cards' of
+          Nothing ->
+            round
+              { hands = hands',
+                phase =
+                  Playing
+                    playing
+                      { cards = cards'
+                      }
+              }
+          Just trick ->
+            round
+              { hands = hands',
+                leader = Trick.winner trick,
+                phase =
+                  Playing
+                    playing
+                      { variant = Variant.next variant,
+                        cards = cards',
+                        tricks = trick : tricks
+                      }
+              }
+    where
+      player = current round
+      cards' = cards ++ [card]
+      hands' = over (Vector.ix player) (Set.delete card) hands
+
+chooseVariant :: JassNat n => Variant -> Round n -> Either Error (Round n)
+chooseVariant variant round@Round {phase} = case phase of
+  Choosing ->
+    pure $
+      round
+        { phase = Playing $ RoundPlaying variant [] []
+        }
+  Playing _ -> Left VariantAlreadyDefined
