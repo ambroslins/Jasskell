7ed76f6f186b71c7c024ec68dcf5e794991ca846
ambroslins <ambros.lins@gmail.com>
2022-04-08 00:13:07 +0200

    add shuffle and deck deal function

--- src/Jass.hs
+++ src/Jass.hs
@@ -2,10 +2,18 @@
 
 module Jass where
 
-import Card (Card)
+import Card (Card (..), Cards, Rank (..), Suit (..), deck)
+import Control.Exception (assert)
 import Control.Monad.Free (MonadFree, liftF)
 import Control.Monad.Free.TH (makeFree)
+import Control.Monad.ST (runST)
+import Data.Finite (Finite, finite, getFinite)
+import Data.Set qualified as Set
+import Data.Vector.Mutable.Sized qualified as Vector.Mutable
+import Data.Vector.Sized (Vector)
+import Data.Vector.Sized qualified as Vector
 import GHC.TypeLits (Div, type (+), type (-))
+import System.Random (RandomGen, uniformR)
 import Variant (Variant)
 import View (Phase (..), Views)
 
@@ -18,4 +26,40 @@ makeFree ''Jass
 
 type MonadJass n m = (JassNat n, MonadFree (Jass n) m)
 
-type JassNat n = (KnownNat n, KnownNat (Div 36 n), n ~ ((n - 1) + 1))
+class (KnownNat n, KnownNat (Div 36 n), n ~ ((n - 1) + 1)) => JassNat n where
+  deal :: RandomGen g => g -> (Vector n Cards, g)
+  deal = dealDeck deck
+
+instance JassNat 3
+
+instance JassNat 4
+
+instance JassNat 5 where
+  deal = dealDeck (Set.delete (Card Bells Six) deck)
+
+instance JassNat 6
+
+instance JassNat 7 where
+  deal = dealDeck (Set.delete (Card Bells Six) deck)
+
+dealDeck :: (KnownNat n, RandomGen g) => Cards -> g -> (Vector n Cards, g)
+dealDeck cards gen = Vector.withSizedList (toList cards) $ \cs ->
+  let (shuffled, g) = shuffle gen cs
+      (n, rest) = Vector.length cs `divMod` Vector.length decks
+      decks = Vector.unfoldrN (splitAt n) (Vector.toList shuffled)
+   in assert (rest == 0) (Vector.map Set.fromList decks, g)
+
+shuffle :: (KnownNat n, RandomGen g) => g -> Vector n a -> (Vector n a, g)
+shuffle gen xs = runST $ do
+  ys <- Vector.thaw xs
+  let randomSwap g i _ =
+        let (j, g') = uniformFiniteR (i, maxBound) g
+         in Vector.Mutable.swap ys i j $> g'
+  gen' <- Vector.ifoldM' randomSwap gen xs
+  zs <- Vector.freeze ys
+  pure (zs, gen')
+
+uniformFiniteR :: (KnownNat n, RandomGen g) => (Finite n, Finite n) -> g -> (Finite n, g)
+uniformFiniteR (low, high) gen =
+  let (i, gen') = uniformR (getFinite low, getFinite high) gen
+   in (finite i, gen')
