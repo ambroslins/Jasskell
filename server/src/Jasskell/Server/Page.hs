2ad20db13d19e58593ed040076b4ee3d65c775d6
ambroslins <ambros.lins@gmail.com>
2022-09-14 15:53:21 +0200

    Fix static paths and also serve index at /play/...

--- server/src/Jasskell/Server/Page.hs
+++ server/src/Jasskell/Server/Page.hs
@@ -8,6 +8,7 @@ import Web.Twain qualified as Twain
 routes :: [Http.Route]
 routes =
   [ Http.get "/" $ html index
+  , Http.get "/play/:tableID" $ html index
   ]
 
 html :: Monad m => HtmlT m a -> m Response
@@ -22,9 +23,9 @@ makeHead :: Monad m => Text -> HtmlT m ()
 makeHead title =
   head_ $ do
     title_ $ toHtml title
-    link_ [rel_ "stylesheet", type_ "text/css", href_ "style.css"]
-    script_ [src_ "main.js"] ("" :: String)
+    link_ [rel_ "stylesheet", type_ "text/css", href_ "/style.css"]
+    script_ [src_ "/main.js"] ("" :: String)
 
 initElm :: Monad m => HtmlT m ()
 initElm =
-  script_ [src_ "index.js"] ("" :: String)
+  script_ [src_ "/index.js"] ("" :: String)
